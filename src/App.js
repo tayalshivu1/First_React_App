294942bd7ce74a180560c9ec52e81031056c2e6a
tayalshivu1 <92094383+tayalshivu1@users.noreply.github.com>
2023-07-10 13:29:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+function ButtonComponent() {
+  function hancleClick() {
+    return alert("I am Clicked...");
+  }
+
+  return <button onClick={hancleClick}>I'm Button</button>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My First React Github</h1>
+      <ButtonComponent />
+    </div>
+  );
+}
